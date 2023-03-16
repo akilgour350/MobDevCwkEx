69a5d7ba14d8d2875e4f54d701c174c5ede1a958
Alastair Kilgour <akilgour350@gmail.com>
2023-03-16 19:35:01 +0000

    need to fix intents bug

---
+++ app/src/main/java/com/adk/kilgour_alastair_s2221119/MoreInfo.java
@@ -0,0 +1,14 @@
+package com.adk.kilgour_alastair_s2221119;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MoreInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_more_info);
+    }
+}
